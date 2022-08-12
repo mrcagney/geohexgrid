be1ccb0f80cf3538054694d764bf5c6a4fbfa47f
Alex Raichev <araichev@mrcagney.com>
2022-08-12 14:54:24 +1200

    Updated test data

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,10 +5,18 @@ from .context import geohex, pytest, DATA_DIR
 from geohex import *
 
 
-GHS = GeoHexSystem("epsg:2193", 50, 0, 0)
+R = 50
+GHS = GeoHexSystem("epsg:2193", R, 0, 0)
 
 
 # Test helper functions
+def test_axial_center_to_id():
+    assert axial_center_to_id(26, 530) == "26-530"
+
+def test_cell_id_to_axial_center():
+    a, b = 2, 3
+    assert id_to_axial_center(axial_center_to_id(a, b)) == (a, b)
+
 def test_axial_to_cartesian():
     get = axial_to_cartesian(0, 0, 5)
     expect = (0, 0)
@@ -40,23 +48,23 @@ def test_hexagon_vertex():
 
 # Test Cell methods
 def test_center():
-    assert Cell(GHS, 0, 0).center() == (0, 0)
+    assert Cell(0, 0, R).center() == (0, 0)
 
 
 def test_vertices():
-    v = Cell(GHS, 0, 0).vertices()
+    v = Cell(0, 0, R).vertices()
     assert len(set(v)) == 6
 
 
 def test_polygon():
-    p = Cell(GHS, 1, 2).polygon()
+    p = Cell(1, 2, R).polygon()
     assert isinstance(p, sg.Polygon)
     # Polygon should have correct area
-    assert np.allclose(p.area, 3 * sqrt(3) / 2 * GHS.R**2)
+    assert np.allclose(p.area, 3 * sqrt(3) / 2 * R**2)
 
 
 def test_neighbour():
-    c = Cell(GHS, 0, 0)
+    c = Cell(0, 0, R)
     assert c.neighbour(2) == c.neighbour(-4)
     assert c.neighbour(0) == c.neighbour("ru")
     assert len(set(c.neighbor(i) for i in range(6))) == 6
@@ -75,15 +83,12 @@ def test_cell_from_axial_point():
 
 
 def test_cell_from_point():
-    R = GHS.R
     c = GHS.cell_from_point(1.1 * R, -0.1 * R)
     assert isinstance(c, Cell)
     assert (c.a, c.b) == (1, -1)
 
 
 def test_grid_from_bbox():
-    R = GHS.R
-
     # One cell
     grid = GHS.grid_from_bbox(0, 0, 0, 0, as_gdf=False)
     assert [c.center_axial() for c in grid] == [(0, 0)]
